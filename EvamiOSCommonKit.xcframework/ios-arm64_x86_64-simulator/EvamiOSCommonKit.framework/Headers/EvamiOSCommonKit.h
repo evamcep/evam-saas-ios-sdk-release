e3c42f92d9b6577646a8c7836d771ce74de27bf2
sametyabalak <samety@evam.com>
2024-08-12 16:11:56 +0300

    publish new version

---
+++ EvamiOSCommonKit.xcframework/ios-arm64_x86_64-simulator/EvamiOSCommonKit.framework/Headers/EvamiOSCommonKit.h
@@ -0,0 +1,18 @@
+//
+//  EvamiOSCommonKit.h
+//  EvamiOSCommonKit
+//
+//  Created by Tuğrul on 18.04.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EvamiOSCommonKit.
+FOUNDATION_EXPORT double EvamiOSCommonKitVersionNumber;
+
+//! Project version string for EvamiOSCommonKit.
+FOUNDATION_EXPORT const unsigned char EvamiOSCommonKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EvamiOSCommonKit/PublicHeader.h>
+
+
